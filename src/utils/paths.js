b150e15c05988ca66ebcd483ccb26fea7cd5b570
Vu Tran <vu@vu-tran.com>
2016-09-05 15:58:27 -0700

    add getPluginPath()

--- src/utils/paths.js
+++ src/utils/paths.js
@@ -6,3 +6,11 @@ exports.DEXT_PATH = path.resolve(exports.USER_PATH, '.dext');
 exports.THEME_PATH = path.resolve(exports.DEXT_PATH, 'plugins');
 exports.CORE_PLUGIN_PATH = path.resolve(__dirname, '..', 'resources', 'plugins');
 exports.PLUGIN_PATH = path.resolve(exports.DEXT_PATH, 'plugins');
+
+/**
+ * Retrieve the path to the plugin
+ *
+ * @param {String} plugin - The name of the plugin/package
+ * @return {String}
+ */
+exports.getPluginPath = plugin => path.resolve(exports.PLUGIN_PATH, plugin);
