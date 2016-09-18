5b811fb2f0c3df26fd047c9fabfd9d22ad4c128d
Vu Tran <vu.tran@thermofisher.com>
2016-09-18 01:21:09 -0700

    add tests for set/get theme and config

--- __tests__/api/index.js
+++ __tests__/api/index.js
@@ -1,4 +1,5 @@
-import api from '../../src/api';
+import api from '../../src/api'; // eslint-disable-line import/imports-first
+import { ERR_MODULE_NOT_INSTALLED } from '../../src/errors'; // eslint-disable-line import/imports-first
 
 describe('api', () => {
   it('should should check for a valid plugin', async () => {
@@ -7,5 +8,21 @@ describe('api', () => {
 
   it('should should check for an invalid plugin', async () => {
     expect(await api.checkOnNpm('INVALID')).toBeTruthy();
+  });
+
+  it('should set a theme', async () => {
+    try {
+      await api.setTheme('foobar');
+    } catch (err) {
+      expect(err.toString()).toEqual(`Error: ${ERR_MODULE_NOT_INSTALLED}`);
+    }
+  });
+
+  it('should get a theme', async () => {
+    expect(await api.getTheme()).toEqual('');
+  });
+
+  it('should get the config', async () => {
+    expect(await api.getConfig()).toEqual([]);
   });
 });
