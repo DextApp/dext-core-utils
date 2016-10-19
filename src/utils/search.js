0e8e581b41dde1df1e8bad73f44f61c49b057891
Maarten den Heyer <mdenheyer@gmail.com>
2016-10-19 18:50:14 +0200

    - Search now return an array of package names - Tried adding tests...

---
+++ src/utils/search.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const getSearchUrl = pkg => `http://npmsearch.com/query\?q\=${pkg}%20AND%20\(keywords:dext-theme%20OR%20keywords:dext-plugin\)\&fields\=name`;
+
+/**
+ * Searches for package marked with the keywords 'dext-plugin' or 'dext-theme'
+ *
+ * @param {String} pkg - The name of the npm package
+ * @return {Promise} - The search results
+ */
+const searchPackages = (pkg) => new Promise((resolve) => {
+  let body = '';
+  // Retrieve the search results
+  return http.get(getSearchUrl(pkg), (res) => {
+    res.on('data', (chunk) => {
+      body += chunk;
+    });
+    res.on('end', () => {
+      const results = JSON.parse(body);
+      // Return the results part of the HTTP response
+      resolve(results.results);
+    });
+  });
+});
+
+module.exports = {
+  getSearchUrl,
+  searchPackages
+};
