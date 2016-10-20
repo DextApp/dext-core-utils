285d841706c15f6f9297cbd8e54b76d72305c48f
Vu Tran <vu@vu-tran.com>
2016-10-20 14:19:04 -0700

    include descriptions; and flatten resolved data

--- __tests__/utils/search.js
+++ __tests__/utils/search.js
@@ -6,22 +6,34 @@ describe('search', () => {
   it('should seach for packages', async () => {
     const mockHttpResponse = {
       results: [
-        { name: ['foobar-default-theme'] },
-        { name: ['foobar-default-plugin'] },
+        {
+          name: ['foobar-default-theme'],
+          description: ['Foobar default theme'],
+        },
+        {
+          name: ['foobar-default-plugin'],
+          description: ['Foobar default plugin'],
+        },
       ],
     };
     require('http').__setMockResponse(JSON.stringify(mockHttpResponse));
 
-    const pkg = 'foobar';
+    const q = 'foobar';
     const mockResponse = [
-      { name: ['foobar-default-theme'] },
-      { name: ['foobar-default-plugin'] },
+      {
+        name: 'foobar-default-theme',
+        desc: 'Foobar default theme',
+      },
+      {
+        name: 'foobar-default-plugin',
+        desc: 'Foobar default plugin',
+      },
     ];
-    expect(await search.searchPackages(pkg)).toEqual(mockResponse);
+    expect(await search.searchPackages(q)).toEqual(mockResponse);
   });
 
   it('should retrieve the package url', () => {
     const pkg = 'foobar';
-    expect(search.getSearchUrl(pkg)).toEqual('http://npmsearch.com/query?q=foobar%20AND%20(keywords:dext-theme%20OR%20keywords:dext-plugin)&fields=name');
+    expect(search.getSearchUrl(pkg)).toEqual('https://npmsearch.com/query?q=foobar%20AND%20(keywords:dext-theme%20OR%20keywords:dext-plugin)&fields=name,description');
   });
 });
